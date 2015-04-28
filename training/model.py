880e1d6ddee89cb5b5a2fd78b20d191ef694b303
Joe DeRose <jderose@stanford.edu>
2015-04-27 17:07:32 -0700

    fixed cdf transposition bug

--- training/model.py
+++ training/model.py
@@ -4,6 +4,7 @@ import scipy as sp
 from bisect import bisect_left
 from sklearn import gaussian_process
 from sklearn import ensemble
+from sklearn.neighbors import KernelDensity
 from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import r2_score, make_scorer
@@ -17,6 +18,7 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import munge
 import pickle 
+
 
 class Model:
     __metaclass__ = ABCMeta
@@ -68,6 +70,18 @@ class Model:
 
         self.php = hcounts/pcounts
 
+    def assignHalo(self, fvec):
+        
+        choice = random.random()
+        ii = []
+        for i in range(self.php.shape[1]):
+            ii.append(bisect_left(edges[i], fvec[i]))
+        
+        ii = np.array(ii)
+        if choice<=self.php[ii]:
+            return True
+        else:
+            return False
 
         
 class HistGauss(Model):
@@ -119,7 +133,10 @@ class HistGauss(Model):
         bins = []
 
         for i in range(len(histarray.shape)):
+            #subsample the data to speed up adaptive binning
             t = random.sample(histarray[:,i],100000)
+            #t = histarray[:,i]
+
             b = bayesian_blocks(t)
             bins.append(b)
 
@@ -180,9 +197,8 @@ class HistGauss(Model):
             pX[:,i] = grid[i].ravel()
 
         pdf = self.reg.predict(pX)
-        pdf = pdf.reshape(np.shape(grid)[1:])
+        pdf = pdf.reshape(np.shape(grid)[1:][::-1])
 
-        #Assuming uniform binning
         dx = self.edges[-1][1:]-self.edges[-1][:-1]
         weights = pdf*dx
         
@@ -220,8 +236,6 @@ class HistGauss(Model):
         print(dbins)
         print(nsel)
         print(len(self.edges[0]))
-
-
 
         for i,d in enumerate(dbins):
             if i==(len(dbins)-1): continue
@@ -277,6 +291,7 @@ class RF(Model):
             rndf = ensemble.RandomForestRegressor()
             reg = GridSearchCV(rndf,param_grid,cv=cv,scoring=scorer,n_jobs=-1)
             reg.fit(self.X_train, self.y_train)
+            print('Fit successful')
         else:
             try:
                 reg = ensemble.RandomForestRegressor(n_jobs=-1)
@@ -298,18 +313,94 @@ class RF(Model):
         """                                                                                                                                                               Use the model to predict values using the provided feature vector                                                                                                 """
         
         return self.reg.predict(fvec)
-        
+
 
     def visModel(self):
 
         f, ax = plt.subplots(2)
-        ax[0].set_yscale('log', nonposy='clip')
-        ax[1].set_yscale('log', nonposy='clip')
-
         pred = self.reg.predict(self.X_test)
-        
+
+        print('Plotting fit')
         nii = np.random.choice(np.arange(len(pred)), 10000, replace=False)
-        sns.kdeplot(self.X_test[nii].ravel(),pred[nii],label='Prediction',ax=ax[0])
-        sns.kdeplot(self.X_test[nii].ravel(),self.y_test[nii], label='Truth', ax=ax[1])
+        ii0, = np.where((pred[nii]==pred[nii]) & (pred[nii]!=0))
+        ii1, = np.where((self.y_test[nii]==self.y_test[nii]) & (self.y_test[nii]!=0))
+        sns.kdeplot(self.X_test[nii][ii0].ravel(),np.log10(pred[nii][ii0]),label='Prediction',ax=ax[0])
+        sns.kdeplot(self.X_test[nii][ii1].ravel(),np.log10(self.y_test[nii][ii1]), label='Truth', ax=ax[1])
         plt.legend()
 
+
+
+class KDE(Model):
+    
+    
+    def preprocess(self):
+
+        nbins = 100
+        arrays = [self.hfeatures, self.pred]
+        histarray = munge.join_rec_arrays(arrays)
+        self.histarray = histarray.view((np.float, len(histarray.dtype.names)))
+        
+        counts, edges = np.histogramdd(self.histarray, bins=nbins, normed=True)
+        
+        self.edges = edges
+        centers = [[(edges[i][j]+edges[i][j+1])/2 for j in range(len(edges[i])-1)] for i in range(len(edges))]
+        
+        npts = 1
+        for i in range(len(centers)):
+            npts*=len(centers[i])
+            
+        self.X = np.ndarray((npts,len(centers)))
+        self.y = counts.flatten()
+        
+        temp = np.meshgrid(*centers)
+        for i in range(len(centers)):
+            self.X[:,i] = temp[i].flatten()
+
+
+    def train(self, cv=None):
+
+        self.hist_train, self.hist_test = train_test_split(self.histarray, test_size=0.5, random_state=0)
+        
+        if cv==None:
+            bandwidth = 1e8
+            #rtol = 1e-8
+            kde = KernelDensity(bandwidth=bandwidth)
+            kde.fit(self.histarray)
+            self.reg = kde
+            
+        else:
+            param_grid = {'bandwidth':np.logspace(1,12,10)}
+            kde = GridSearchCV(KernelDensity(),param_grid,cv=cv)
+            kde.fit(self.hist_train)
+
+
+    def visModel(self, suptitle=None):
+
+        ncuts = 3
+        f, ax = plt.subplots(ncuts,ncuts)
+        sX = np.sort(np.unique(self.X[:,0]))
+        dbins = np.logspace(np.log10(np.min(sX)),np.log10(np.max(sX)),ncuts**2+1)
+        print(dbins)
+        for i,d in enumerate(dbins):
+            if i==(len(dbins)-1): continue
+            xii = np.where((dbins[i]<=self.X[:,0]) & (self.X[:,0]<dbins[i+1]))
+            log_pdf = self.reg.score_samples(self.X[xii])
+            pred = np.exp(log_pdf)
+            truth = self.y[xii]
+
+            ax[i/ncuts,i%ncuts].set_xscale("log", nonposx='clip')
+            ax[i/ncuts,i%ncuts].plot(self.X[xii][:,1],pred,label='Prediction')
+            ax[i/ncuts,i%ncuts].plot(self.X[xii][:,1],truth, label='Truth')
+            ax[i/ncuts,i%ncuts].set_title('Density = {0:.2f}'.format(d))
+            plt.legend()
+
+        if suptitle!=None:
+            f.suptitle(suptitle)
+
+        plt.tight_layout()
+
+    
+    def predict(self):
+        pass
+
+    
